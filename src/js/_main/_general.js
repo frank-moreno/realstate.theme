dbb50be3962291bacd4f7f7792c1a7d45e014f09
frank-moreno <hello@frank-moreno.com>
2023-12-08 20:12:03 +0000

    js header menu scrolling fixed

--- src/js/_main/_general.js
+++ src/js/_main/_general.js
@@ -3,6 +3,23 @@ var $ = jQuery;
 
 $(document).ready(function () {
 
+    // add class on scroll down to add styles to header
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('header').addClass('scrolled');
+        } else {
+            $('header').removeClass('scrolled');
+        }
+    }
+    );
+
+    // add class to homepages header to add styles to header
+
+    if ($('body').hasClass('home')) {
+        $('.wp-block-navigation__responsive-container ul li').addClass('homepage-menu');
+        $('.wp-block-site-title a').addClass('homepage-logo');
+    }
+
     $(window).scroll(startCounter);
 
     function startCounter() {
--- src/js/_main/_main.js
+++ src/js/_main/_main.js
@@ -2,6 +2,5 @@
 //import js files
 
 import './_general.js';
-import './_header.js';
 
 import './_swiper.js';
